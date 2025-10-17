e5c046cd17cd869ccdf322a68f57b0df8f632f92
anupamgaur <anupamgaur02@gmail.com>
2025-10-17 20:33:21 +0530

    added address

--- src/main/java/in/anupam/models/Address.java
+++ src/main/java/in/anupam/models/Address.java
@@ -1,5 +1,41 @@
 package in.anupam.models;
 
 public class Address {
+    private String street;
+    private String houseNumber;
+    private String postalCode;
+    private String city;
 
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(String houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
 }
